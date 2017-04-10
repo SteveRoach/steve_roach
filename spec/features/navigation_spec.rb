0020255feb612a5d742639f0c31c7ef43bffbe77
steveroach <steve_roach@hotmail.com>
2017-04-10 23:51:20 +1000

    Navigation

---
+++ spec/features/navigation_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"  
+ 
+RSpec.feature "Site navigation", :type => :feature do  
+    scenario "'/' navigation goes to the Home page" do  
+        visit "/" 
+        click_link('', :href => '/') 
+        expect(page).to have_title('Cat Play') 
+    end 
+ 
+    scenario "'/cp_journal' navigation goes to the Journal page" do  
+        visit "/" 
+        click_link('', :href => '/cp_journal') 
+        expect(page).to have_title('Cat Play | Journal') 
+    end 
+ 
+    scenario "'/cp_technical' navigation goes to the Technical page" do  
+        visit "/" 
+        click_link('', :href => '/cp_technical') 
+        expect(page).to have_title('Cat Play | Technical') 
+    end 
+ 
+    scenario "'/cp_contact' navigation goes to the Contact page" do  
+        visit "/" 
+        click_link('', :href => '/cp_contact') 
+        expect(page).to have_title('Cat Play | Contact') 
+    end 
+ 
+end  
